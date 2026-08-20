212897aaa0e6fc296bd766993d824de3e7610220
soham3301 <splint3r777@gmail.com>
2026-08-20 21:19:44 +0530

    46 - yt_music_playlist_half_done

---
+++ 46_yt_music_playlist/main.py
@@ -0,0 +1,22 @@
+
+import json
+import requests
+from bs4 import BeautifulSoup
+
+# URL = "https://appbrewery.github.io/bakeboard-hot-100/2026-03-28/"
+
+# response = requests.get(URL)
+# result = response.text
+
+# soup = BeautifulSoup(result, "html.parser")
+
+# songs = soup.find_all(name="h3")
+
+# song_list = []
+
+# for song in songs:
+#     song_list.append(song.getText())
+
+# print(song_list)
+
+#! Not Completed - YT Browser session / request header error in  chromium / No login in chrome or firefox
